5806434082bee9960e7cadcac4045de28739a322
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-03-05 14:23:56 +0100

    fix the outputs

--- app/Logger/LogAsJSON.php
+++ app/Logger/LogAsJSON.php
@@ -6,6 +6,8 @@ class LogAsJSON implements Logger
 {
 	public function render($data)
 	{
-		var_dump(json_encode($data, JSON_PRETTY_PRINT));
+		header('Content-Type: application/json');
+		
+		echo json_encode($data, JSON_PRETTY_PRINT);
 	}
 }
--- app/Logger/LogAsXML.php
+++ app/Logger/LogAsXML.php
@@ -2,32 +2,14 @@
 
 namespace App\Logger;
 
+use Spatie\ArrayToXml\ArrayToXml;
+
 class LogAsXML implements Logger
 {
 	public function render($data)
 	{
-		// creating object of SimpleXMLElement
-		$xml_data = new \SimpleXMLElement('<?xml version="1.0"?><data></data>');
+		header('Content-Type: text/xml');
 
-		// function call to convert array to xml
-		$this->arrayToXML($data,$xml_data);
-
-		//saving generated xml file; 
-		$result = $xml_data->asXML('logs/log.xml');
-	}
-
-	private function arrayToXML($data, &$xml_data)
-	{
-		foreach( $data as $key => $value ) {
-			if (is_array($value)) {
-				if (is_numeric($key)) {
-                	$key = 'item'.$key; //dealing with <0/>..<n/> issues
-           		}
-            	$subnode = $xml_data->addChild($key);
-            	array_to_xml($value, $subnode);
-        	} else {
-        		$xml_data->addChild("$key",htmlspecialchars("$value"));
-        	}
-    	}
+		echo ArrayToXml::convert($data);
 	}
 }
