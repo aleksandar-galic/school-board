e945cde0d5e778cfbca271b69e30c860a6561e6a
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-03-08 19:36:42 +0100

    fix the sql file

--- database.sql
+++ database.sql
@@ -1,31 +1,40 @@
-create table school_board.boards
+create database school_board;
+
+use school_board;
+
+create table boards
 (
-    id   int auto_increment
-        primary key,
+    id int auto_increment primary key,
     name text not null
 );
 
-create table school_board.students
+create table students
 (
-    id       int auto_increment
-        primary key,
-    name     text not null,
-    board_id int  not null,
-    constraint students_boards_id_fk
-        foreign key (board_id) references school_board.boards (id)
+    id int auto_increment primary key,
+    name text not null,
+    board_id int not null
 );
 
-create table school_board.grades
+create table grades
 (
-    id         int auto_increment
-        primary key,
+    id int auto_increment primary key,
     student_id int not null,
-    subject     text not null,
-    value      int not null,
-    constraint grades_students_id_fk
-        foreign key (student_id) references school_board.students (id)
+    subject text not null,
+    value int not null
 );
 
+alter table students
+add foreign key (board_id) references boards(id);
+
+alter table grades
+add foreign key (student_id) references students(id);
+
+-- Populate boards table
+insert into boards (name)
+values ('CSM');
+
+insert into boards (name)
+values ('CSMB');
 
 -- Populate students table
 insert into students (name, board_id)
@@ -35,28 +44,20 @@ insert into students (name, board_id)
 values ('Katarina', 2);
 
 -- Populate grades table
-insert into grades (student_id, value)
+insert into grades (student_id, subject, value)
 values (1, 'languages', 8);
-insert into grades (student_id, value)
+insert into grades (student_id, subject, value)
 values (1, 'mathematics', 6);
-insert into grades (student_id, value)
+insert into grades (student_id, subject, value)
 values (1, 'programming', 7);
-insert into grades (student_id, value)
+insert into grades (student_id, subject, value)
 values (1, 'science', 10);
 
-insert into grades (student_id, value)
+insert into grades (student_id, subject, value)
 values (2, 'languages', 8);
-insert into grades (student_id, value)
-values (2, 'languages', 10);
-insert into grades (student_id, value)
-values (2, 'languages', 9);
-insert into grades (student_id, value)
-values (2, 'languages', 6);
-
--- Populate boards table
-
-insert into boards (name)
-values ('CSM');
-
-insert into boards (name)
-values ('CSMB');
+insert into grades (student_id, subject, value)
+values (2, 'mathematics', 10);
+insert into grades (student_id, subject, value)
+values (2, 'programming', 9);
+insert into grades (student_id, subject, value)
+values (2, 'science', 6);
