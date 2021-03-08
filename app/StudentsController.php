8aa9d8ac80a4e760f742cf20e002bc5269669f78
Aleksandar Galić <aleksandar.galicc@gmail.com>
2021-03-08 12:15:28 +0100

    remove unnecessary code

--- app/StudentsController.php
+++ app/StudentsController.php
@@ -2,19 +2,10 @@
 
 namespace App\Controllers;
 
-use Core\Database;
-use App\Logger\Logger;
 use App\Models\Student;
 
 class StudentsController
 {
-	// protected $logger;
-
-	// public function __construct(Logger $logger)
-	// {
-	// 	$this->logger = $logger;
-	// }
-
 	public function show($id)
 	{
 		$student = new Student();
